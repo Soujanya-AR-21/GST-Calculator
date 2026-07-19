8ec5edd6bc19d728011b48708c823db585742f4a
Soujanya A R <soujanyaar21@gmail.com>
2026-07-19 20:50:42 +0530

    Show history page when opening history

--- script.js
+++ script.js
@@ -104,3 +104,4 @@ function clearHistory() {
 
 function openHistory() {
     document.querySelector(".calculator").style.display = "none"; // hide calculator
+    document.getElementById("historyPage").style.display = "block"; 
